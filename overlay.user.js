6177c9ec9611269ccd0d7b5776ab3628a15ed15f
klorollerde <themo_h@hotmail.de>
2023-07-21 12:49:24 +0200

    Refactoring

--- overlay.user.js
+++ overlay.user.js
@@ -11,7 +11,6 @@
 // ==/UserScript==
 let url = "https://raw.githubusercontent.com/klorollerde/pixel/main/overlay.png"
 let url_de = "https://place.army/overlay_target.png"
-/*let url_uk = "https://raw.githubusercontent.com/klorollerde/pixel/beta/overlay_uk.png"*/
 
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
@@ -30,14 +29,5 @@ if (window.top !== window.self) {
                 i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
                 return i;
             })());
-        /*
-        document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].children[0].getElementsByTagName("garlic-bread-camera")[0].children[0].shadowRoot.children[0].appendChild(
-            (function () {
-                const i = document.createElement("img");
-                i.src = url_uk;
-                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
-                return i;
-            })())
-            */
     }, false);
 }
