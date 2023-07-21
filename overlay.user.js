9569249246c82fb81e12a5314e142cfb2e1714f8
Moritz Hafke <m.hafke@gekos.de>
2023-07-21 11:45:13 +0200

    remove uk test

--- overlay.user.js
+++ overlay.user.js
@@ -11,7 +11,7 @@
 // ==/UserScript==
 let url = "https://raw.githubusercontent.com/klorollerde/pixel/main/overlay.png"
 let url_de = "https://place.army/overlay_target.png"
-let url_uk = "https://raw.githubusercontent.com/klorollerde/pixel/beta/overlay_uk.png"
+/*let url_uk = "https://raw.githubusercontent.com/klorollerde/pixel/beta/overlay_uk.png"*/
 
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
@@ -30,6 +30,7 @@ if (window.top !== window.self) {
                 i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
                 return i;
             })());
+        /*
         document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].children[0].getElementsByTagName("garlic-bread-camera")[0].children[0].shadowRoot.children[0].appendChild(
             (function () {
                 const i = document.createElement("img");
@@ -37,5 +38,6 @@ if (window.top !== window.self) {
                 i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
                 return i;
             })())
+            */
     }, false);
 }
