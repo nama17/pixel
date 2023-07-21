2c05a2713a5d016d1562838bbdb69d57987c6181
Moritz Hafke <m.hafke@gekos.de>
2023-07-21 11:23:02 +0200

    added uk flag

--- overlay.user.js
+++ overlay.user.js
@@ -11,6 +11,7 @@
 // ==/UserScript==
 let url = "https://raw.githubusercontent.com/klorollerde/pixel/main/overlay.png"
 let url_de = "https://place.army/overlay_target.png"
+let url_uk = "https://github.com/Sockenschuh/RplaceOverlay/blob/master/overlay_UK.png"
 
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
@@ -22,12 +23,19 @@ if (window.top !== window.self) {
                 i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
                 return i;
             })());
-            document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].children[0].getElementsByTagName("garlic-bread-camera")[0].children[0].shadowRoot.children[0].appendChild(
-                (function () {
-                    const i = document.createElement("img");
-                    i.src = url_de;
-                    i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
-                    return i;
-                })())
+        document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].children[0].getElementsByTagName("garlic-bread-camera")[0].children[0].shadowRoot.children[0].appendChild(
+            (function () {
+                const i = document.createElement("img");
+                i.src = url_de;
+                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+                return i;
+            })());
+        document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].children[0].getElementsByTagName("garlic-bread-camera")[0].children[0].shadowRoot.children[0].appendChild(
+            (function () {
+                const i = document.createElement("img");
+                i.src = url_uk;
+                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+                return i;
+            })())
     }, false);
 }
